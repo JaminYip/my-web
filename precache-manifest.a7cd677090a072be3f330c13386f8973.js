dc1465ea567649ac355ac27cc3cb6c50a9c14a55
Jamin Yip <jamin.yip4510@gmail.com>
2020-02-04 02:02:32 +0900

    Updates

---
+++ precache-manifest.a7cd677090a072be3f330c13386f8973.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e666b992b9a6edd2a36e564cd8f619b5",
+    "url": "/my-web/index.html"
+  },
+  {
+    "revision": "61275519216993c7f32f",
+    "url": "/my-web/static/css/main.65eaa89a.chunk.css"
+  },
+  {
+    "revision": "8582cb984a70abd18450",
+    "url": "/my-web/static/js/2.07cf2060.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/my-web/static/js/2.07cf2060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61275519216993c7f32f",
+    "url": "/my-web/static/js/main.db5b9246.chunk.js"
+  },
+  {
+    "revision": "f0d2e5218f310d443475",
+    "url": "/my-web/static/js/runtime-main.bc1dd2c9.js"
+  },
+  {
+    "revision": "6b8501089384f3732a5ae13f9d8465f5",
+    "url": "/my-web/static/media/bg.6b850108.jpg"
+  }
+]);
