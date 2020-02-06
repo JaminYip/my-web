f895c8ba867631990c1f3bb1b9784202d0f3ff12
Jamin Yip <jamin.yip4510@gmail.com>
2020-02-07 01:56:52 +0900

    Updates

---
+++ precache-manifest.18f97b57136e9fef71b6c47f55e52848.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cc81144e658095461d5d966c557632",
+    "url": "/my-web/index.html"
+  },
+  {
+    "revision": "0336ca32b3d5bf1f712a",
+    "url": "/my-web/static/css/main.bb3b94bc.chunk.css"
+  },
+  {
+    "revision": "870cfcfcd805264d9251",
+    "url": "/my-web/static/js/2.b7d78e60.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/my-web/static/js/2.b7d78e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0336ca32b3d5bf1f712a",
+    "url": "/my-web/static/js/main.139b2404.chunk.js"
+  },
+  {
+    "revision": "f0d2e5218f310d443475",
+    "url": "/my-web/static/js/runtime-main.bc1dd2c9.js"
+  },
+  {
+    "revision": "3c134aadaee4df50ca46cb4feed0530a",
+    "url": "/my-web/static/media/avatar.3c134aad.jpg"
+  },
+  {
+    "revision": "6b8501089384f3732a5ae13f9d8465f5",
+    "url": "/my-web/static/media/home.6b850108.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-web/precache-manifest.c54c685c8fb4414774b180a9228798d3.js"
+  "/my-web/precache-manifest.18f97b57136e9fef71b6c47f55e52848.js"
 );
 
 self.addEventListener('message', (event) => {
